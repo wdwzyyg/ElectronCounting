89d44cf060a5182ab7174b222a5cb23bbeb5048c
wdwzyyg <jwei74@wisc.edu>
2022-04-14 12:56:31 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ElectronCounting',
+    version='0.1.0',
+    packages=['CountingNN'],
+    url='https://github.com/wdwzyyg/CountingNN.git',
+    license='MIT',
+    author='Jingrui Wei',
+    author_email='jwei74@wisc.edu',
+    description='Count single electron in direct electron detector data'
+    
+)
