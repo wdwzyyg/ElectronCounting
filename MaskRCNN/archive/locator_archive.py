9e086a730d6d9b534bbef4130a12f0462e1f6bb1
wdwzyyg <jwei74@wisc.edu>
2022-11-20 11:51:00 -0600

    fully update locator.py to fix grid line problem

--- MaskRCNN/archive/locator_archive.py
+++ MaskRCNN/archive/locator_archive.py
@@ -1,36 +1,13 @@
+import itertools
+import math
 from typing import List
 
 import numpy as np
 import torch
-import torch.nn.functional as F
 
 
 def map01(mat):
     return (mat - mat.min()) / (mat.max() - mat.min())
-
-
-def stich_windows(windows, k, cropx, cropy):
-    if not torch.is_tensor(windows):
-        windows = torch.as_tensor(windows)
-    row0 = torch.cat([windows[0, 0][:-k, :-k]] +
-                     [win[:-k, k:-k] for win in windows[0, 1:-1]] +
-                     [windows[0, -1][:-k, k:]], dim=1)
-    rows = []
-
-    for r in range(windows.shape[0] - 1):
-        r = r + 1
-        rows.append(torch.cat([windows[r, 0][k:-k, :-k]] +
-                              [win[k:-k, k:-k] for win in windows[r, 1:-1]] +
-                              [windows[r, -1][k:-k, k:]], dim=1)
-                    )
-
-    row_last = torch.cat([windows[-1, 0][k:, :-k]] +
-                         [win[k:, k:-k] for win in windows[-1, 1:-1]] +
-                         [windows[-1, -1][k:, k:]], dim=1)
-
-    final = torch.cat([row0] + rows + [row_last], dim=0)
-    final = final[:cropx, :cropy]
-    return final
 
 
 class Locator:
@@ -55,7 +32,8 @@ class Locator:
         >>>from  MaskRCNN.locator import Locator
         >>>counting = Locator(model_object, device, process_stride, method,
         >>>dark_threshold, locating_model, dynamic_param, p_list = p_list, meanADU=meanADU)
-        >>>filtered =  predict_sequence(inputs)  # inputs as the image tensor in shape [N,h,w]
+        >>>boxes_list = counting.predict(x) # x as the image array in shape [1,h,w]
+        >>>filtered, coords, eventsize = counting.locate(x[0], boxes_list[0])
 
     """
 
@@ -88,8 +66,7 @@ class Locator:
         if limit < 3:  # make the minimum limit as 2.
             limit = 3
         self.fastrcnn_model.rpn._pre_nms_top_n = {'training': limit * self.p_list[0], 'testing': limit * self.p_list[0]}
-        self.fastrcnn_model.rpn._post_nms_top_n = {'training': limit * self.p_list[1],
-                                                   'testing': limit * self.p_list[1]}
+        self.fastrcnn_model.rpn._post_nms_top_n = {'training': limit * self.p_list[1], 'testing': limit * self.p_list[1]}
         self.fastrcnn_model.roi_heads.detections_per_img = int(limit * self.p_list[2])
         self.fastrcnn_model.roi_heads.score_thresh = self.p_list[3] / limit if limit < self.p_list[4] else 0
         self.fastrcnn_model.roi_heads.nms_thresh = 0.02  # smaller, delete more detections
@@ -97,74 +74,49 @@ class Locator:
         if limit > 10:
             self.dark_threshold = 0  # for image that not quite sparse, lift the pre-thresholding.
 
-    def images_to_window_lists(self, inputs: torch.tensor) -> List[torch.tensor]:
-        """
-        transform a batch of images(3D tensor) into windows of the images, with up-sampling by 2.
-        """
-        inputs = inputs.to(self.device)
-        outputs = []
-        maxs= []
-        mins = []
-        for image in inputs:
-            pad = [torch.floor(image.shape[0] / (self.process_stride - 6)) * (
-                        self.process_stride - 6) + self.process_stride,
-                   torch.floor(image.shape[1] / (self.process_stride - 6)) * (
-                               self.process_stride - 6) + self.process_stride]
-            image = F.pad(image, (0, pad[0], 0, pad[0]))
-            windows = image.unfold(0, self.process_stride, self.process_stride - 6)
-            windows = windows.unfold(1, self.process_stride, self.process_stride - 6)
-            # up-sampling the windows
-            windows = torch.nn.Upsample(scale_factor=2, mode='nearest')(windows)
-            outputs.append(list(torch.flatten(windows, start_dim=0, end_dim=1)))
-            maxs.append([image.max()]*(windows.shape[0]*windows.shape[1]))
-            mins.append([image.min()]*(windows.shape[0]*windows.shape[1]))
-
-        return outputs, windows.shape, maxs, mins
-
     @torch.no_grad()
-    def predict_sequence(self, inputs: torch.tensor):
+    def grid_predict(self, inputs: List[torch.tensor]):
         """
         apply model on image one patch after another. The patches size equals the image size in training data, so that
         no need to tune the model detection limits for different limit sizes.
         """
-        self.fastrcnn_model.transform.crop_max = max(inputs.shape[1], inputs.shape[2])
-        # make size_divisible equals process_stride here to avoid inconsistent padding issue.
-        self.fastrcnn_model.transform.size_divisible = self.process_stride
-        self.fastrcnn_model.eval()
+        boxes_list = []
+        for whole_img in inputs:
+            max_size = list(whole_img.shape)
+            max_size[1] = int(math.ceil(float(max_size[1]) / self.process_stride) * self.process_stride)
+            max_size[2] = int(math.ceil(float(max_size[2]) / self.process_stride) * self.process_stride)
 
-        counted_list = []
-        inputs = torch.tensor(inputs, dtype=torch.float32)
-        counted_images = torch.zeros_like(inputs)
+            divisible_img = whole_img.new_full(max_size, 0)
+            divisible_img[: whole_img.shape[0], : whole_img.shape[1], : whole_img.shape[2]].copy_(whole_img)
+            boxes = []
+            for i, j in list(itertools.product(range(int(max_size[1] / self.process_stride)),
+                                               range(int(max_size[2] / self.process_stride)))):
+                image_cell = divisible_img[:, i * self.process_stride:(i + 1) * self.process_stride,
+                             j * self.process_stride:(j + 1) * self.process_stride]
 
-        image_cell_list, windowshape, maxs, mins = self.images_to_window_lists(inputs)
-        for i, image_cell in enumerate(image_cell_list):
-            if self.dynamic_param:
-                self.model_tune(image_cell)
+                if self.dynamic_param:
+                    self.model_tune(image_cell)
 
-            # thresholding to remove dark noise before applying the model
-            image_cell[image_cell < self.dark_threshold] = 0
+                # thresholding to remove dark noise before applying the model
+                image_cell[image_cell < self.dark_threshold] = 0
 
-            image_cell = (image_cell - mins[i]) / (maxs[i] - mins[i])  # norm the image cells equally
-            boxes = self.fastrcnn_model([image_cell])[0]['boxes']
+                # image_cell = map01(image_cell)
+                image_cell = (image_cell - whole_img.min()) / (whole_img.max() - whole_img.min())  # norm the image cells equally
+                output = self.fastrcnn_model([image_cell])[0]['boxes']
+                increment = torch.zeros_like(output)
+                increment[:, 0] = j * self.process_stride
+                increment[:, 2] = j * self.process_stride
+                increment[:, 1] = i * self.process_stride
+                increment[:, 3] = i * self.process_stride
+                boxes.append((output + increment))
 
-            select = []
-            for row, value in enumerate(boxes):
-                if 0 < (value[2] - value[0]) < 30 and 0 < (value[3] - value[1]) < 30:
-                    select.append(row)
-            select = torch.as_tensor(select, dtype=torch.int, device=self.device)
-            filtered_boxes = torch.index_select(boxes, 0, select)
-            filtered_boxes = filtered_boxes / 2.0
+                del image_cell, increment, output
 
-            filtered, _, _ = self.locate(F.interpolate(image_cell, scale_factor=0.5, mode='nearest'), filtered_boxes)
-            counted_list.append(filtered)
+            boxes_list.append(torch.cat(boxes, dim=0))
 
-        for index in range(len(counted_list)/windowshape[0]/windowshape[1]):
-            counted_cells = counted_list[i*windowshape[0]*windowshape[1]:(i+1)*windowshape[0]*windowshape[1]]
-            counted_cells = torch.as_tensor(counted_cells).reshape((windowshape[0], windowshape[1],
-                                                                    windowshape[2]/2, windowshape[3]/2))
-            counted_images[index] = stich_windows(counted_cells, k=3, cropx=inputs.shape[1], cropy=inputs.shape[2])
+            del divisible_img
 
-        return counted_images
+        return boxes_list
 
     def locate(self, image_array, boxes):
 
@@ -195,14 +147,14 @@ class Locator:
             if self.method == 'fcn':
                 if torch.cuda.is_available() and self.device == torch.device('cuda'):
                     self.locating_model.cuda()
-                    image = torch.tensor(np.expand_dims(patch, axis=(0, 1)), dtype=torch.float32).cuda()
+                    image = torch.tensor(np.expand_dims(patch, axis=(0, 1)), dtype=torch.float).cuda()
                     image = image.cuda()
                     self.locating_model.eval()
                     with torch.no_grad():
                         res = self.locating_model.forward(image)
                     res = res.cpu()
                 else:
-                    image = torch.tensor(np.expand_dims(patch, axis=(0, 1)), dtype=torch.float32)
+                    image = torch.tensor(np.expand_dims(patch, axis=(0, 1)), dtype=torch.float)
                     self.locating_model.eval()
                     with torch.no_grad():
                         res = self.locating_model.forward(image)
@@ -233,3 +185,35 @@ class Locator:
             filtered[(coords[:, 0], coords[:, 1])] = 1
 
         return filtered, coords, eventsize
+
+    def predict(self, x: np.ndarray) -> List[torch.tensor]:
+        """
+        input:x: [n, w, h]
+        output: list of [N,4]
+        """
+        self.fastrcnn_model.transform.crop_max = x.shape[1]
+        # make size_divisible equals process_stride here to avoid inconsistent padding issue.
+        self.fastrcnn_model.transform.size_divisible = self.process_stride
+        self.fastrcnn_model.eval()
+        images = []
+        out_list = []
+        for i, im in enumerate(x):
+            # image = map01(im)
+            image = torch.tensor(im, dtype=torch.float32)
+            image = image[None, None, ...]
+            image = torch.nn.Upsample(scale_factor=2, mode='bilinear')(image)
+            images.append(image[0])  # return dimension [C, H, W]
+
+        inputs = list(im_.to(self.device) for im_ in images)
+        boxes_list = self.grid_predict(inputs)
+        for boxes in boxes_list:
+            select = []
+            for row, value in enumerate(boxes):
+                if 1 < (value[2] - value[0]) < 30 and 1 < (value[3] - value[1]) < 30:
+                    select.append(row)
+            select = torch.as_tensor(select, dtype=torch.int, device=self.device)
+            filtered_boxes = torch.index_select(boxes, 0, select)
+            filtered_boxes = filtered_boxes / 2.0
+            out_list.append(filtered_boxes)
+
+        return out_list
