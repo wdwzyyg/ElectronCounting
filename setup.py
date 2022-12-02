dda1dcd64969be8ea0b035eeefbd9c4d4e225fb3
wdwzyyg <jwei74@wisc.edu>
2022-12-02 13:29:49 -0600

    fill in details for PYPI release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ElectronCounting',
-    version='v0.1.0',
+    version='0.1.0',
     packages=find_packages(),
     url='https://github.com/wdwzyyg/ElectronCounting.git',
     download_url='https://github.com/wdwzyyg/ElectronCounting/archive/refs/tags/v0.1.0.tar.gz',
