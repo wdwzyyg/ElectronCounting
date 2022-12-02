85bdd5009159743fe6c97bfbc95937ed86493f82
wdwzyyg <jwei74@wisc.edu>
2022-12-02 17:33:34 -0600

    latest version for PYPI release

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='ElectronCounting',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/wdwzyyg/ElectronCounting.git',
-    download_url='https://github.com/wdwzyyg/ElectronCounting/archive/refs/tags/v0.1.0.tar.gz',
     license='MIT',
     author='Jingrui Wei',
     author_email='jwei74@wisc.edu',
@@ -19,13 +18,12 @@ setup(
         "Faster R-CNN",
     ],
     install_requires=[
-        "setuptools~=62.0.0",
-        "torch~=1.12.0",
-        "numpy~=1.22.3",
-        "scikit-learn~=1.0.2",
-        "matplotlib~=3.5.1",
-        "opencv-python~=4.5.5.64",
-        "torchvision~=0.14.0"
+        "torch==1.12.0",
+        "torchvision==0.13.0",
+        "numpy>=1.20.1",
+        "scikit-learn>=1.0.2",
+        "matplotlib>=3.2.2",
+
     ],
 
 )
