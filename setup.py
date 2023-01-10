41eea44fa975bdc22a032b7b129c8b9b8e074e3b
wdwzyyg <jwei74@wisc.edu>
2023-01-10 15:34:59 -0600

    re-add the model weight

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ElectronCounting',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     url='https://github.com/wdwzyyg/ElectronCounting.git',
     license='MIT',
